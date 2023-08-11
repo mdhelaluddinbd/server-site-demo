f923a21a2deb2bd0e13bba4c11ddf571fd84048b
Md.Helal Uddin <mdhelaluddin.cse@gmail.com>
2023-08-11 10:43:52 +0600

    Remove /api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", apiRoute);
+app.use("/", apiRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://127.0.0.1:${port}`);
